2ec055452d11375eb4bf2ee3936b9d63791f86a7
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-08-02 02:54:45 +0900

    remove not null from users.email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140801171423) do
+ActiveRecord::Schema.define(version: 20140801175147) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id",       null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20140801171423) do
 
   create_table "users", force: true do |t|
     t.string   "name",                    null: false
-    t.string   "email",                   null: false
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "uid",        default: "", null: false
