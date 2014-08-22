aa66acd458e4a12925cbdd7226f2c6d8aae8bb01
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-08-22 18:26:02 +0900

    fix spec

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,33 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe ActivitiesController, :type => :controller do
+  before do
+    @user = create(:user)
+  end
 
   describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to be_success
+    context 'signed out' do
+      before do
+        test_signout
+        get :index
+      end
+
+      it { expect(response).to redirect_to(signin_path) }
     end
+
   end
 
   describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to be_success
+    context 'signed out' do
+      before do
+        test_signout
+        get :new
+      end
+
+      it { expect(response).to redirect_to(signin_path) }
     end
+
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to be_success
+  describe "POST create" do
+    context 'signed out' do
+      before do
+        test_signout
+        post :create, activity: {}
+      end
+
+      it { expect(response).to redirect_to(signin_path) }
     end
+
   end
 
   describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to be_success
+    context 'signed out' do
+      before do
+        test_signout
+        get :show, id: 1
+      end
+
+      it { expect(response).to redirect_to(signin_path) }
     end
   end
-
 end
